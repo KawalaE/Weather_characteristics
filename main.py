7633b251b754f85ea9ce37f86cd4a185f4e65b4b
KawalaE <edyta.wer.kawala@gmail.com>
2022-08-25 21:52:19 +0200

    Added cities and created connector.

--- main.py
+++ main.py
@@ -1,12 +1,42 @@
 import requests
 import json
-from time import time
-
-latitude = 50.055375
-longitude = 19.92981
+import mysql.connector
+from time import time, sleep
 
 with open('api_key.txt') as f:
     API_KEY = f.readline()
+
+with open('db_pass.txt') as f:
+    DB_PASS = f.readline()
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password=DB_PASS
+)
+
+print(mydb)
+
+# Key: City, Value: (latitude, longitude)
+cities = {
+    "Cracow": (50.055375, 19.92981),
+    "Warsaw": (52.233687, 21.009258),
+    "Poznan": (52.409985, 16.897515),
+    "Wroclaw": (51.107187, 17.031505)
+}
+
+
+def start_collecting_data():
+    while 1:
+        for city in cities:
+            receive_data = get_data_from_server(cities[city][0], cities[city][1], API_KEY)
+            ct, t, p, h = extract_data_from_response(receive_data)
+            save_to_db(city, ct, t, p, h)
+        sleep(5)
+
+
+def save_to_db(c, ct, t, p, h):
+    pass
 
 
 def get_data_from_server(lat: float, lon: float, api_key: str) -> json:
@@ -15,14 +45,10 @@ def get_data_from_server(lat: float, lon: float, api_key: str) -> json:
     return data
 
 
-#print(json.dumps(request.json(), indent=4))
-
 def extract_data_from_response(response_data: json):
     temp = response_data["main"]["temp"] - 273.15 # convert from Kelvin to Celsius
     pressure = response_data["main"]["pressure"]
     humidity = response_data["main"]["humidity"]
     current_time = time()
-    return current_time, temp, pressure, humidity
+    return int(current_time), int(temp*100), pressure, humidity
 
-
-print(extract_data_from_response(get_data_from_server(latitude, longitude, API_KEY)))
